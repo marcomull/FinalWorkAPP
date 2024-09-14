c5239d2d2dcdb50b219bc459fbe504ca1e1e85d6
marcomull <105877026+marcomull@users.noreply.github.com>
2024-09-14 14:25:26 -0500

    Uploading progress structure

---
+++ src/Routes/Routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+function RoutesComponent() {
+    return (
+        <BrowserRouter>
+            <Routes>
+
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default RoutesComponent;
